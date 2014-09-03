fc67ee7a0229221b2bbca66484834ccea0c01ab0
xushaowei <991598414@qq.com>
2014-09-03 15:57:09 +0800

    删除原来投票按钮接口

--- uservote/userVote.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once '../Config/dynamicobject.php';
-require_once './'.dynamicobject :: getShortDymanicObject('userVoteImpl').'.php'; 
-$className = dynamicobject :: getShortDymanicObject('userVoteImpl');
-$class=new ReflectionClass($className);
-$obj = $class->newInstance();
-$retObj = $obj->getUserVoteObj();
-$obj->transfer($retObj);
-?>
