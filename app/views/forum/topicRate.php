6811910a6f5ac97518e112873dc28900d4426b8f
hanpengyu <hanpengyu521@126.com>
2014-10-09 11:20:59 +0800

    no message

--- app/views/forum/topicRate.php
+++ app/views/forum/topicRate.php
@@ -7,198 +7,190 @@
 <meta http-equiv="Cache-Control" content="no-transform" />
 <link rel="alternate" type="application/vnd.wap.xhtml+xml" media="handheld" href="target"/>
 <meta name="viewport" content="width=device-width,user-scalable=no,minimum-scale=1.0,initial-scale=1.0">
+<script type="text/javascript" src ="<?php echo $this->dzRootUrl; ?>/static/js/common.js"></script>
 <script type="text/javascript" src="<?php echo $this->rootUrl.'/js/'; ?>jquery-2.0.3.min.js"></script>
 <style type="text/css">
-*{
-    margin: 0;
-    padding: 0;
-    word-wrap: break-word;
-}
-h1, h2, h3, h4, h5, h6 {
-    font-size: 1em;
-}
-a {
-    color: #333;
-    text-decoration: none;
-}
-li {
-    list-style: none;
-}
-body, input, button, select, textarea, p{
-    font: 12px/1.5 "Hiragino Sans GB", "Microsoft YaHei", "微软雅黑", "宋体", Arial, Verdana, sans-serif;
-    color: #444;
-}
-.delect, .zhiding {
-    width: 320px;
-    margin: 0 auto;
-}
-table {
-    border-collapse: collapse;
-    display: table;
-    border-color: gray;
-    empty-cells: show;
-}
+    *{
+        margin: 0;
+        padding: 0;
+        word-wrap: break-word;
+    }
+    body, input, button, select, textarea {
+        font: 12px/1.5 "Hiragino Sans GB", "Microsoft YaHei", "微软雅黑", "宋体", Arial, Verdana, sans-serif;
+        color: #444;
+    }
+    h1, h2, h3, h4, h5, h6 {
+        font-size: 1em;
+    }
+    a {
+        color: #333;
+        text-decoration: none;
+    }
+    li {
+        list-style: none;
+    }
+    #main{
+        margin: 0 auto;
+        width: 20%;
+        height: auto;
+        /*background: #e3e3e3;*/
+        padding-bottom: 5px;
+    }
 
-#tab1{
-    width: 320px;
-    /*background: #e3e3e3;*/
-    border-radius: 5px;
-    text-align: center;
-    /*border: 1px solid red;*/
-}
+    table td {
+        border-radius: 5px;
+    }
 
-#div1{
-    margin:20px auto;
-    width: 320px;
-    /*background: #e3e3e3;*/
-    border-radius: 5px;
-    height: 100%;
-}
-select{
-    border-radius: 5px;
-}
+    table tr {
+    	height: 25px;
+    }
 
-#reason{
-    width: 320px;
-    margin-top:5px;
-    margin-bottom: 5px;
-}
+    .rate_font{
+        font-size: 15px;
+        font-weight: 700;
+        color: #369;
+    }
 
-#yes{
-    width: 40px;
-    height: 25px;
-    border-radius: 5px;
-}
-    
-#bu1{
-    text-align: right;
-    line-height: 20px;
-}
-.list{
-    padding: 5px 0 5px 5px;
+	select{
+	    border-radius: 5px;
+	}
 
-}
+    #rate_category {
+    	margin:15px 0px 5px 0px;
+    }
 
-.flb {
-    font-size: 14px;
-    font-weight: 700;
-    color: #369;
-    padding-left: 10px;
-}
-.pc{
-    vertical-align: middle;
-    margin: 0 5px 1px 0;
-    padding: 0;    
-}
-.pn {
-    width: 80px;
-    height: 32px;
-    line-height: 32px;
-    background: #6da136;
-    color: #fff;
-    display: inline-block;
-    padding: 0;
-    -webkit-border-radius: 3px;
-    -moz-border-radius: 3px;
-    border-radius: 3px;
-    margin: 5px 0;
-    border: none;
-}
+	#rate_reason {
+		width: 100%;
+		padding: 3px;
+	}
 
-.labeltxt{
-    color:#09C;
-}
+	#foot {
+		width: 100px,
+		padding:3px;
+		text-align: right;
+	}
 
-textarea{
-    margin-top: 5px;
-    resize:none;
-    border-radius: 5px;
-}
+	.pc {
+	    vertical-align: middle;
+	    margin: 0 8px 1px 0;
+	    padding: 0;   		
+	}
 
-@media (max-width: 480px) {
-#div1, #reason {
-    width: 90%;
-    margin: 5px auto;
-    padding-left: 5%;
-    padding-right: 5%;
-}
-.tpclg .pt {
-    width: 97%;
-}
-}
+	.btu {
+	    width: 80px;
+	    height: 32px;
+	    line-height: 32px;
+	    background: #6da136;
+	    color: #fff;
+	    display: inline-block;
+	    padding: 0;
+	    -webkit-border-radius: 3px;
+	    -moz-border-radius: 3px;
+	    border-radius: 3px;
+	    margin: 5px 0;
+	    border: none;
+	}	
+
+	.pt {
+		width: 100%;
+		margin-top: 5px;
+		border-radius: 5px;
+		padding: 4px;
+	}
+
+    @media (max-width: 480px) {
+	    #main{
+	        width: 90%;
+	        margin: 0 auto;
+	        padding-left: 5%;
+	        padding-right: 5%;
+	    }
+
+	    #rate_category, #rate_reason, #foot {
+	    	width: 90%;
+	    }
+	}
 </style>
 <script type="text/javascript">
-$(function () {
-    $('#reasonSelect').on({'change': function () {
-        var value = $(this).val();
-        if (value != '-1') {
-            if (value == '0') {
-                $('#reason').get(0).focus();
-            } else {
-                $('#reason').val(value);
-            }
+    $(function(){
+    	$('select').on({
+    		change:function(){
+    			$(this).prev().val($(this).val());
+    		}
+    	});
+    	
+	    $('#reasonSelect').on({'change': function () {
+	        var value = $(this).val();
+	        if (value != '-1') {
+	            if (value == '0') {
+	                $('#reason').get(0).focus();
+	            } else {
+	                $('#reason').val(value);
+	            }
+	        }
+	    }});
+
+        var errorMsg = '<?php echo WebUtils::u($errorMsg); ?>';
+        if (errorMsg != '') {
+            alert(errorMsg);
         }
-    }});
-    var errorMsg = '<?php echo WebUtils::u($errorMsg); ?>';
-    if (errorMsg != '') {
-        alert(errorMsg);
-    }
-});
+    })
 </script>
 </head>
 <body>
-    <div id="div1">
-        <form method="post" action="<?php echo $formUrl; ?>">
-            <h4 class="flb">评分</h4>
-            <table id="tab1">
-                <tr>
+    <div id="main">
+    <form method="post" action="<?php echo $formUrl; ?>">
+        <h4 class="rate_font">评分</h4>
+        <div id="rata_interval">
+            <table style="margin:0 auto;text-align:center;width:100%" >
+                <tr class="rate_top">
                     <td colspan="2"></td>
-                    <td>评分区间</td>
-                    <td>今日剩余</td>
+                    <td width=25%>评分区间</td>
+                    <td width=25%>今日剩余</td>
                 </tr>
-            <?php foreach( $ratelist as $id=>$options): ?>
+                <?php foreach( $ratelist as $id=>$options): ?>
                 <?php
                     $reg = '/[+|-][\w]+/';
                     preg_match_all($reg, $options, $options);
                     $options = $options[0];
                 ?>
-                <!-- [modify]去掉对于评分今日剩余为0的判断 -->
-                <?php //if($options[$id] != ''): ?>
-                    <tr>
-                        <td><label class="labeltxt"><?php echo WebUtils::u($_G['setting']['extcredits'][$id]['title']);?></label></td>
-                        <td class="list">
-                            <select name="score<?php echo $id ?>">
-                                    <option value="">0</option>
-                                <?php foreach($options as $key=>$value): ?>
-                                    <option value="<?php echo $value; ?>"><?php echo $value; ?></option>
-                                <?php endforeach; ?>
-                            </select>
-                        </td>
-                        <td><?php echo $_G['group']['raterange'][$id]['min'];?> ~ <?php echo $_G['group']['raterange'][$id]['max'];?></td>
-                        <td><?php echo $maxratetoday[$id] ?></td>
-                    </tr>
-                <?php //endif; ?>
-            <?php endforeach; ?>
-            </table>
-            <br />
-            <span>可选评分理由:</span>
-            <select id="reasonSelect">
-                <?php foreach($selectreason as $value): ?>
-                    <option value="<?php echo WebUtils::u($value); ?>"><?php echo WebUtils::u($value); ?></option>
+                <tr class="rate_content">
+                    <td ><?php echo WebUtils::u($_G['setting']['extcredits'][$id]['title']);?></td>
+                    <td width=35%>
+                        <input type="text" style="width:30%;" value="0" name="score<?php echo $id ?>" />
+                        <select class="rate_size">
+                            <option value="0">0</option>
+                            <?php foreach($options as $key=>$value): ?>
+                                <option value="<?php echo $value; ?>"><?php echo $value; ?></option>
+                            <?php endforeach; ?>
+                    	</select>
+                    </td>
+                    <td><?php echo $_G['group']['raterange'][$id]['min'];?> ~ <?php echo $_G['group']['raterange'][$id]['max'];?></td>
+                    <td><?php echo $maxratetoday[$id] ?></td>
+                </tr>
                 <?php endforeach; ?>
-                    <option value="0">自定义</option>
+            </table>
+        </div>
+
+        <div id="rate_category">
+        	<span>可选评分理由:</span>
+            <select id="reasonSelect">
+            <?php foreach($selectreason as $value): ?>
+                <option value="<?php echo WebUtils::u($value); ?>"><?php echo WebUtils::u($value); ?></option>
+            <?php endforeach; ?>
+                <option value="0">自定义</option>
             </select>
-            <p><textarea id="reason" name="reason" class="pt" rows="3"></textarea></p>
+            <p><textarea id="reason" name="reason" class="pt" rows="2"></textarea></p>
             <p class="o pns">
                 <label for="sendreasonpm">
                     <input type="checkbox" name="sendreasonpm" id="sendreasonpm" class="pc" style="margin-right:5px;">通知作者
                 </label>
             </p>
             <p style="text-align:center">
-                <button type="submit" name="modsubmit" id="modsubmit" class="pn pnc" value="确定" ><span>确定</span>
+                <button type="submit" name="modsubmit" id="modsubmit" class="btu" value="确定" ><span>确定</span>
                 </button>
             </p>
-        </form>
+        </div>
+    </form>
     </div>
 </body>
-</html>
