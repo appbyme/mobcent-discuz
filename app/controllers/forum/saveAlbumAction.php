433af8b994117397fbe86834ea7bf6db22280e31
hanpengyu <hanpengyu521@126.com>
2014-12-11 11:21:28 +0800

    修改保存相册

--- app/controllers/forum/saveAlbumAction.php
+++ app/controllers/forum/saveAlbumAction.php
@@ -8,7 +8,6 @@
 * @param int $pidId 相册图片的id
 * @param string $pidDesc 相册图片的描述
 * @param int $albumId 相册ID -1 为默认相册
-* @param string $interval $ids、$picDescs字符串的分隔符 默认为英文的逗号
 */
 
 if (!defined('IN_DISCUZ') || !defined('IN_APPBYME')) {
@@ -16,25 +15,22 @@ if (!defined('IN_DISCUZ') || !defined('IN_APPBYME')) {
 }
 
 class saveAlbumAction extends MobcentAction {
-    public function run($ids, $picDescs, $albumId=-1, $interval=',') {
+    public function run($ids, $picDesc, $albumId=-1) {
         $res = WebUtils::initWebApiArray();
-        $res = $this->_saveAlbum($ids, $picDescs, $albumId, $interval);
+        $res = $this->_saveAlbum($ids, $picDesc, $albumId, $interval);
         echo WebUtils::outputWebApi($res, '', false);
     }
 
-    private function _saveAlbum($ids, $picDescs, $albumId, $interval) {
+    private function _saveAlbum($ids, $picDesc, $albumId) {
         global $_G;
         $ids = rawurldecode($ids);
-        $picDescs = rawurldecode($picDescs);
-        $interval = rawurldecode($interval);
+        $picDesc = rawurldecode($picDesc);
+        $pidIdArray = explode(',', $ids);
 
-        $pidIdArray = explode($interval, $ids);
-        $descArray = explode($interval, $picDescs);
-        $conditions = array_combine($pidIdArray, $descArray);
-        
-        foreach ($conditions as $picid => $desc) {
-            C::t('home_pic')->update_for_uid($_G['uid'], $picid, array('title' => $desc, 'albumid' => $albumId));
+        foreach ($pidIdArray as $picid) {
+            C::t('home_pic')->update_for_uid($_G['uid'], $picid, array('title' => $picDesc, 'albumid' => $albumId));
         }
+
         require_once libfile('function/spacecp');
         album_update_pic($albumId);
         return $this->makeErrorInfo($res, 'mobcent_save_album_success', array('noError'=>1));
