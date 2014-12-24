b27ba2eefd72d7c0e5054046ef96f8b9f7bd2572
hanpengyu <hanpengyu521@126.com>
2014-12-24 17:31:39 +0800

    修改门户列表采用距离排序的时候丢失参数的问题（$longitude', $latitude, $radius）

--- app/controllers/portal/NewsListAction.php
+++ app/controllers/portal/NewsListAction.php
@@ -14,7 +14,7 @@ if (!defined('IN_DISCUZ') || !defined('IN_APPBYME')) {
 class NewsListAction extends MobcentAction {
     const HANDPAGE = 10;
     public function run($moduleId, $page = 1, $pageSize = 10, $longitude='', $latitude='', $radius=100000, $isImageList=0) {
-        $getKeyArray = array('mid' => $moduleId, 'page' => $page, 'pageSize' => $pageSize, 'isImageList' => $isImageList);
+        $getKeyArray = array('mid' => $moduleId, 'page' => $page, 'pageSize' => $pageSize, 'longitude' => $longitude, 'latitude' => $latitude, 'radius' =>$radius, 'isImageList' => $isImageList);
         $filter = unserialize(AppbymePoralModule::getModuleParam($moduleId));
         if (is_array($filter['other_filter']) || $filter['topic_orderby'] == 'distance') {
             $res = $this->getResult($getKeyArray);
