0760576019686795389ab0a540325882b208d8bb
hanpengyu <hanpengyu521@126.com>
2015-01-23 14:25:53 +0800

    1、统一微信登录和qq登录的数据结构。 2、添加绑定qq登录。

--- app/controllers/user/SavePlatFormInfoAction.php
+++ app/controllers/user/SavePlatFormInfoAction.php
@@ -18,79 +18,57 @@ class SavePlatFormInfoAction extends MobcentAction {
         $email = WebUtils::t(rawurldecode($email));
         $res = $this->initWebApiArray();
         $res = $this->getPlatFormInfo($res, $username, $oauthToken, $password, $openId, $email, $gender, $act, $platformId);
-        echo WebUtils::outputWebApi($res, '', false);   
+        echo WebUtils::outputWebApi($res, '', false);
     }
 
     public function getPlatFormInfo($res, $username, $oauthToken, $password, $openId, $email, $gender, $act, $platformId) {
         if ($platformId == 20) {
-            $res = $this->_saveInfo($res, $username, $email, $oauthToken, $openId, $gender, $platformId);
+            $res = $this->_saveQqInfo($res, $username, $oauthToken, $password, $openId, $email, $gender, $act, $platformId);
         } elseif ($platformId == 30) {
             $res = $this->_saveWxInfo($res, $username, $oauthToken, $password, $openId, $email, $gender, $act, $platformId);
         }
         return $res;
     }
 
-    private function _saveInfo($res, $username, $email, $oauthToken, $openId, $gender, $platformId) {
-        $username = WebUtils::t(rawurldecode($username));
-        $email = WebUtils::t(rawurldecode($email));
-        $password = MOBCENT_HACKER_PASSWORD;
-        $regInfo = UserUtils::register($username, $password, $email, 'qq');
-        if ($regInfo['errcode']) {
-            return $this->makeErrorInfo($res, $regInfo['message']);
+    private function _saveQqInfo($res, $username, $oauthToken, $password, $openId, $email, $gender, $act, $platformId) {
+
+        if ($act == 'register') {
+            $regInfo = UserUtils::register($username, $password, $email, 'qq');
+            if ($regInfo['errcode']) {
+                return $this->makeErrorInfo($res, $regInfo['message']);
+            }
+
+            $uid = $regInfo['info']['uid'];
+            $userInfo = UserUtils::getUserInfo($uid);
+            $userAccess = AppbymeUserAccess::registerProcess($uid, $password);
+
+            $this->_updateQqMember($uid, $oauthToken, $openId, $gender);
+
+            $res['body']['token'] = (string)$userAccess['token'];
+            $res['body']['secret'] = (string)$userAccess['secret'];
+            $res['body']['uid'] = (int)$regInfo['info']['uid'];
+            return $res;
+        } elseif ($act == 'bind') {
+            
+            global $_G;
+            $logInfo = UserUtils::login($username, $password);
+            if ($logInfo['errcode']) {
+                UserUtils::delUserAccessByUsername($username);
+                return $this->makeErrorInfo($res, 'mobcent_bind_error');
+            }
+
+            $this->_updateQqMember($_G['uid'], $oauthToken, $openId, $gender);
+
+            $userInfo = AppbymeUserAccess::loginProcess($_G['uid'], $password);
+            $userAvatar = UserUtils::getUserAvatar($_G['uid']);
+            $res['body']['token'] = (string)$userInfo['token'];
+            $res['body']['secret'] = (string)$userInfo['secret'];
+            $res['body']['uid'] = (int)$_G['uid'];
+            $res['body']['avatar'] = (string)$userAvatar;
+            $res['body']['userName'] = (string)$_G['username'];
+            return $res;
         }
-
-        $uid = $regInfo['info']['uid'];
-        $userInfo = UserUtils::getUserInfo($uid);
-        $userAccess = AppbymeUserAccess::registerProcess($uid, $password);
-
-        if (!empty($platformId) && $platformId == 20) {
-            $qqdata = array(
-                'uid' => $uid,
-                'conuin' => $oauthToken,
-                'conuinsecret' =>'',
-                'conopenid' => $openId,
-                'conisfeed' => 1,
-                'conispublishfeed' => 1,
-                'conispublisht' => 1,
-                'conisregister' => 1,
-                'conisqzoneavatar' => 1,
-                'conisqqshow' => 1,
-                );
-            $qqbind = array('mblid'=>'','uid'=>$uid,'uin' =>$openId,'type'=>1,'dateline'=>time());
-            $this->_inserBindlog($qqbind);
-            $this->_inserConnect($qqdata);
-            $updateInfo = array('avatarstatus' => 1, 'conisbind' => 1); // 用户是否绑定QQ
-            DzCommonMember::updateMember($updateInfo, array('uid' => $uid));
-            $setarr ['gender'] = intval($gender);
-            C::t ('common_member_profile')->update($uid, $setarr);
-
-            $ipArray = explode('.', $_G['clientip']);
-            $sid = FileUtils::getRandomFileName('',6);
-            $data = array(
-                'sid' => $sid,
-                'ip1' => $ipArray[0],
-                'ip2' => $ipArray[1],
-                'ip3' => $ipArray[2],
-                'ip4' => $ipArray[3],
-                'uid' => $userInfo['uid'],
-                'username' =>$userInfo['username'],
-                'groupid' => $userInfo['groupid'],
-                'invisible' =>'0',
-                'action' => '' ,
-                'lastactivity' => time(),
-                'fid' => '0',
-                'tid' => '0',
-                'lastolupdate' => '0'
-                );
-                DzCommonSession::insertComSess($data);
-                require_once libfile('cache/userstats', 'function');
-                build_cache_userstats();
-
-                $res['token'] = (string)$userAccess['token'];
-                $res['secret'] = (string)$userAccess['secret'];
-                $res['uid'] = (int)$regInfo['info']['uid'];
-                return $res;
-        }
+        
         // 客户端参数不正确
         return $this->makeErrorInfo($res, 'mobcent_error_params');
 
@@ -102,6 +80,50 @@ class SavePlatFormInfoAction extends MobcentAction {
 
     private function _inserConnect($data) {
         return DbUtils::getDzDbUtils(true)->insert('common_member_connect', $data);
+    }
+
+    private function _updateQqMember($uid, $oauthToken, $openId, $gender) {
+        global $_G;
+        $qqdata = array(
+            'uid' => $uid,
+            'conuin' => $oauthToken,
+            'conuinsecret' =>'',
+            'conopenid' => $openId,
+            'conisfeed' => 1,
+            'conispublishfeed' => 1,
+            'conispublisht' => 1,
+            'conisregister' => 1,
+             'conisqqshow' => 1,
+            );
+        $qqbind = array('mblid'=>'','uid'=>$uid,'uin' =>$openId,'type'=>1,'dateline'=>time());
+        $this->_inserBindlog($qqbind);
+        $this->_inserConnect($qqdata);
+        $updateInfo = array('avatarstatus' => 1, 'conisbind' => 1); // 用户是否绑定QQ
+        DzCommonMember::updateMember($updateInfo, array('uid' => $uid));
+        $setarr ['gender'] = intval($gender);
+        C::t ('common_member_profile')->update($uid, $setarr);
+
+        $ipArray = explode('.', $_G['clientip']);
+        $sid = FileUtils::getRandomFileName('',6);
+        $data = array(
+            'sid' => $sid,
+            'ip1' => $ipArray[0],
+            'ip2' => $ipArray[1],
+            'ip3' => $ipArray[2],
+            'ip4' => $ipArray[3],
+            'uid' => $userInfo['uid'],
+            'username' =>$userInfo['username'],
+            'groupid' => $userInfo['groupid'],
+            'invisible' =>'0',
+            'action' => '' ,
+            'lastactivity' => time(),
+            'fid' => '0',
+            'tid' => '0',
+            'lastolupdate' => '0'
+            );
+            DzCommonSession::insertComSess($data);
+            require_once libfile('cache/userstats', 'function');
+            build_cache_userstats();
     }
 
     private function _saveWxInfo($res, $username, $oauthToken, $password, $openId, $email, $gender, $act, $platformId) {
@@ -128,7 +150,7 @@ class SavePlatFormInfoAction extends MobcentAction {
             $logInfo = UserUtils::login($username, $password);
             if ($logInfo['errcode']) {
                 UserUtils::delUserAccessByUsername($username);
-                return $this->makeErrorInfo($res, $logInfo['message']);
+                return $this->makeErrorInfo($res, 'mobcent_bind_error');
             }
 
             $data = array('uid' => $_G['uid'], 'openid' => $openId, 'status' => 1, 'type' => 1);
@@ -136,14 +158,13 @@ class SavePlatFormInfoAction extends MobcentAction {
 
             $userInfo = AppbymeUserAccess::loginProcess($_G['uid'], $password);
             $userAvatar = UserUtils::getUserAvatar($_G['uid']);
-            $res['token'] = (string)$userInfo['token'];
-            $res['secret'] = (string)$userInfo['secret'];
-            $res['uid'] = (int)$_G['uid'];
-            $res['avatar'] = (string)$userAvatar;
-            $res['userName'] = (string)$_G['username'];
+            $res['body']['token'] = (string)$userInfo['token'];
+            $res['body']['secret'] = (string)$userInfo['secret'];
+            $res['body']['uid'] = (int)$_G['uid'];
+            $res['body']['avatar'] = (string)$userAvatar;
+            $res['body']['userName'] = (string)$_G['username'];
         }
         return $res;
     }
-            
 
 }
